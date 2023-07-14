4b3f1c2ffa945893bb5b9304f02b13d504b5378b
kamilahasenova <221890@astanait.edu.kz>
2023-07-14 14:42:19 +0600

    first init

--- AverageFinder.java
+++ AverageFinder.java
@@ -1,36 +1,28 @@
 import java.util.Scanner;
 
-public class MinimumFinder {
-    public static int findMinimum(int[] arr) {
+public class AverageFinder {
+    public static double findAverage(int[] arr) {
         if (arr == null || arr.length == 0) {
             throw new IllegalArgumentException("Array must not be empty or null.");
         }
-
-        int min = arr[0];
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
         }
-
-        return min;
+        return (double) sum / arr.length;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = scanner.nextInt();
-
         int[] array = new int[n];
-
         for (int i = 0; i < n; i++) {
             array[i] = scanner.nextInt();
         }
 
-        int minimum = findMinimum(array);
-        System.out.println(minimum);
-
+        double average = findAverage(array);
+        System.out.println(average);
         scanner.close();
     }
 }
